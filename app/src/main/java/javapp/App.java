00b0bb4cef111d04ffe1deb9665bdf8d5adb49fe
Bruno Casali <brunoocasali@gmail.com>
2023-05-04 19:10:21 -0300

    Working with Java 17

--- app/src/main/java/javapp/App.java
+++ app/src/main/java/javapp/App.java
@@ -3,15 +3,41 @@
  */
 package javapp;
 
-import com.meilisearch.sdk.Client;
-import com.meilisearch.sdk.Config;
+import com.meilisearch.sdk.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
         try {
+            JSONArray array = new JSONArray();
+            ArrayList items = new ArrayList() {{
+                add(new JSONObject().put("id", "1").put("title", "Carol").put("genres",new JSONArray("[\"Romance\",\"Drama\"]")));
+                add(new JSONObject().put("id", "2").put("title", "Wonder Woman").put("genres",new JSONArray("[\"Action\",\"Adventure\"]")));
+                add(new JSONObject().put("id", "3").put("title", "Life of Pi").put("genres",new JSONArray("[\"Adventure\",\"Drama\"]")));
+                add(new JSONObject().put("id", "4").put("title", "Mad Max: Fury Road").put("genres",new JSONArray("[\"Adventure\",\"Science Fiction\"]")));
+                add(new JSONObject().put("id", "5").put("title", "Moana").put("genres",new JSONArray("[\"Fantasy\",\"Action\"]")));
+                add(new JSONObject().put("id", "6").put("title", "Philadelphia").put("genres",new JSONArray("[\"Drama\"]")));
+            }};
+
+            array.put(items);
+            String documents = array.getJSONArray(0).toString();
+
             Client client = new Client(new Config("http://meilisearch:7700", "masterKey"));
 
             System.out.println(client.health());
+            
+            Index index = client.index("movies");
+
+            // If the index 'movies' does not exist, Meilisearch creates it when you first add the documents.
+            index.addDocuments(documents);
+
+            Thread.sleep(1000);
+
+            System.out.println(client.index("movies").search("pi"));
         } catch (Exception e) {
             System.out.println(e);
         }
